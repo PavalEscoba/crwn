0f7a7c4a77bfcd0a11e131d92f00fe4a87cb95a8
Pavel Metlitsky <p.metlitsky@itransition.com>
2021-07-23 18:53:52 +0300

    add: shop page component + CollectionPreview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,4 +1,19 @@
-імпортім реакт
-імпортім стілі
-функ.компонент
-вяртаем
+import React from 'react';
+
+import './collection-preview.styles.scss';
+
+
+const CollectionPreview = ({title, items}) => (
+  <div className="collection-preview">
+    <h2 className="title">{title.toUpperCase()}</h2>
+    <div className="preview">
+      {items
+        .filter((item, i) => i > 4)
+        .map(item => (
+          <div key={item.id}>{item.name}</div>
+        ))}
+    </div>
+  </div>
+);
+
+export default CollectionPreview;
